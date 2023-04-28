7266fc552b38569a1bdcecf7cf2291aef889caa6
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-04-28 21:30:07 +0300

    init: initialize and add background image

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="bg-my-blueberry w-screen h-screen">
+	<img src="/assets/bg.jpg" class="bg absolute top-0 right-0 h-full" alt="background image" />
+  <div class="absolute inset-0 z-10 bg-gradient-to-l from-my-blueberry via-transparent via-30% to-my-blueberry w-screen h-screen"></div>
+  <slot />
+</div>
+
+<style>
+  .bg {}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				'my-blueberry': '#000102',
+				'my-darkerblue': '17315C',
+				'my-darkblue': '0A43A1',
+				'my-blue': '026CDA',
+				'my-lightblue': '#37A1EC',
+				'my-white': '#F5F9F7'
+			}
+		}
+	},
+
+	plugins: [forms, typography]
+};
+
+module.exports = config;
