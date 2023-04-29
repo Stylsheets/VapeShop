55a2df5fe28cb269dc6a31eb073e08dae3ed1f92
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-04-29 15:41:29 +0300

    feat: added social media icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,31 @@
 <script>
-  import Hero from '$components/Hero.svelte';
+	import Hero from '$components/Hero.svelte';
+	import Instagram from '../components/Icons/Instagram.svelte';
+	import Twitter from '../components/Icons/Twitter.svelte';
+	import Youtube from '../components/Icons/Youtube.svelte';
 </script>
 
-<div>
-  <div class="flex justify-between">
-    <div class="h-[45rem] flex flex-col justify-center items-center">
-      <Hero />
-    </div>
-    <div></div>
-  </div>
+<div class="flex flex-col">
+	<div class="flex justify-between">
+		<div class="h-[38rem] flex flex-col justify-center items-center">
+			<Hero />
+		</div>
+		<div />
+	</div>
+	<div class="flex flex-col w-full h-40">
+		<div class="flex flex-col justify-between h-full">
+			<div />
+			<div class="flex gap-3 mt-auto">
+				<a class="hover:opacity-50" href="#">
+					<Instagram />
+				</a>
+				<a class="hover:opacity-50" href="#">
+					<Twitter />
+				</a>
+				<a class="hover:opacity-50" href="#">
+					<Youtube />
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
